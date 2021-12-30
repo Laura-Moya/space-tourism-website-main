adafc914594e3d40ad8484791d7c624ed5621096
laura <l.moya@alumnos.upm.es>
2021-12-31 00:12:05 +0100

    Las imágenes y las descripción ya se cargan sólo

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,12 +1,33 @@
+var marsDescription = "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
+var europaDescription = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin."
+var titanDescription= "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn."
 
-function underlineSelectedOption(link){
+var marsDistance = "225 mil. km";
+var europaDistance = "628 mil. km";
+var titanDistance = "1.6 bil. km";
 
-  switch(link.innerHTML)
-  {
-    case 'MOON': break;
-    case 'MARS': window.location.href = '../starter-code/destination-mars.html';
-    case 'EUROPA': window.location.href = '../starter-code/destination-europa.html';
-    case 'TITAN': window.location.href = '../starter-code/destination-titan.html';
-  }
+var marsTravelTime = "9 months";
+var europaTravelTime = "3 years";
+var titanTravelTime = "7 years";
 
+
+function loadData(link)
+{
+  var nombrePlaneta = link.innerHTML.toLowerCase();
+  // Rellenamos la imagen del planeta
+  document.getElementById("imagenPlaneta").src = "assets/destination/image-"+nombrePlaneta+".webp";
+  // Rellenamos el nombre del planeta
+  document.getElementById("tituloPlaneta").innerHTML = nombrePlaneta.toUpperCase();
+  // Rellenamos la descripcion
+  document.getElementById("descripcionPlaneta").innerHTML = window[nombrePlaneta.concat('Description')];
+  // Rellenamos la distancia
+  document.getElementById("distance").innerHTML = window[nombrePlaneta.concat('Distance')];
+  // Rellenamos el tiempo estimado
+  var travelTime = nombrePlaneta+'TravelTime';
+  document.getElementById("travelTime").innerHTML = window[nombrePlaneta.concat('travelTime')];
+
+}
+
+function redirectPage(link){
+  loadData(link);
 }
